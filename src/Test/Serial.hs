e66e21c5132047c45ec04b31b36af13fcde7d519
Scott Murphy <scottmurphy09@gmail.com>
2014-12-22 22:18:35 -0600

    Updated Tests

--- src/Test/Serial.hs
+++ src/Test/Serial.hs
@@ -1,3 +1,7 @@
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE QuasiQuotes #-}
 {- |
 Module      :  Test.Serial
 Description :  Test.Serial run serialization tests against static files
@@ -6,20 +10,20 @@ License     :  MIT License
 Maintainer  :  Scott Murphy
 Stability   :  unstable 
 Portability :   non-portable (System.Posix)
+
+
 -}
 
 
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE QuasiQuotes #-}
-
 module Test.Serial (runAesonSerializationTest
                    , runBinarySerializationTest
+                   , runCerealSerializationTest
                    , TestError (..) ) where
 
 import   qualified Data.Aeson as A
-
 import   qualified Data.Binary as B
+import   qualified Data.Serialize as C
+import qualified Data.ByteString as BStrict
 import qualified Data.ByteString.Lazy as BLazy
 import           GHC.Generics
 import Data.String.Here.Interpolated (i)
@@ -28,7 +32,8 @@ import           System.IO (withFile, IOMode(..),hIsEOF)
 
 data TestError = NoFileFound |  -- NoFileFound could simply mean it is the first time the test was ran
                  AesonError  String |
-                 BinaryError String
+                 BinaryError String |
+                 CerealError String                  
       deriving (Generic,Read,Show,Eq,Ord)
 
 instance A.ToJSON TestError where
@@ -62,19 +67,20 @@ runAesonSerializationTest dataUnderTest file = withFile file ReadWriteMode creat
         (Right a) 
           |(A.toJSON . makeMockAesonInference $ a) == (A.toJSON.makeMockAesonInference $ dataUnderTest)
            -> return . Right $ a
-          |otherwise -> return . Left . AesonError . explainError  (A.toJSON . makeMockAesonInference $ a) $ (A.toJSON.makeMockAesonInference $ dataUnderTest)
+          |otherwise -> return . Left . AesonError . explainError (A.toJSON . makeMockAesonInference $ a) $ (A.toJSON . makeMockAesonInference $ dataUnderTest)
 
     writeOutputAndExit h = do
       putStrLn "file not found, writing given serialization to disk, rerun tests"
       BLazy.hPut h $ A.encode dataUnderTest
       return . Left $ NoFileFound
 
-
 explainError old new = [i|
   JSON doesn't match: 
    old:  ${old}
    new:  ${new}
   |]
+
+
 
 
 -- | 'MockBinaryInference' just to force two inferred types to be the same  
@@ -99,7 +105,7 @@ runBinarySerializationTest dataUnderTest file = withFile file ReadWriteMode crea
     createBinarySerializeTest' h = do
       binaryByteString <- BLazy.hGetContents h
       case B.decodeOrFail binaryByteString of
-        -- (Left s) -> return . Left .  BinaryError $ " whee"
+        (Left s) -> return . Left .  BinaryError . show $ s
         (Right (_,_,a) )
           |(B.encode . makeMockBinaryInference $ a) == (B.encode.makeMockBinaryInference $ dataUnderTest)
            -> return . Right $ a
@@ -110,4 +116,41 @@ runBinarySerializationTest dataUnderTest file = withFile file ReadWriteMode crea
       BLazy.hPut h $ B.encode dataUnderTest
       return . Left $ NoFileFound
 
+
+
+
+
+
+-- | 'MockCerealInference' just to force two inferred types to be the same  
+newtype MockCerealInference a = MockCerealInference a
+   deriving (Generic)
+
+instance C.Serialize a => C.Serialize (MockCerealInference a) where             
+
+
+makeMockCerealInference :: (C.Serialize a) => a -> MockCerealInference a
+makeMockCerealInference testVal = MockCerealInference testVal
+
+runCerealSerializationTest :: (C.Serialize a) => a -> FilePath -> IO (Either TestError a)
+runCerealSerializationTest dataUnderTest file = withFile file ReadWriteMode createCerealSerializeTest
+ where
+    createCerealSerializeTest h = do
+      aNewFile <- hIsEOF h
+      if aNewFile
+        then writeOutputAndExit h
+        else createCerealSerializeTest' h
+             
+    createCerealSerializeTest' h = do
+      cerealByteString <- BStrict.hGetContents h
+      case C.decode cerealByteString of
+        (Left s) -> return . Left .  CerealError $ s
+        (Right a)
+          |(C.encode . makeMockCerealInference $ a) == (C.encode.makeMockCerealInference $ dataUnderTest)
+           -> return . Right $ a
+          |otherwise -> return . Left . CerealError $ "Serializations do not match"
+  
+    writeOutputAndExit h = do
+      putStrLn "file not found, writing given serialization to disk, rerun tests"
+      BStrict.hPut h $ C.encode dataUnderTest
+      return . Left $ NoFileFound
 
--- test/Test/SerialSpec.hs
+++ test/Test/SerialSpec.hs
@@ -1,17 +1,24 @@
+
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Test.SerialSpec (main, spec) where
 
 import Control.Applicative
+
 import Data.Aeson
 import GHC.Generics
 import Control.Monad.IO.Class
 import Test.Hspec
+import   qualified Data.Binary as B
+import   qualified Data.Serialize as C
+
+
 import Test.QuickCheck.Monadic
 import Test.QuickCheck
 import Test.Serial ( runAesonSerializationTest
                    , runBinarySerializationTest
+                   , runCerealSerializationTest
                    , TestError (..) )
 import Filesystem 
 
@@ -20,14 +27,17 @@ data TestSerialData = TestSerialData {
                              anExampleIntField    :: Int,
                              anExampleListTupleField   :: (Int,Int)
                              }
-
                       deriving (Generic,Eq)
 
 testAesonSerialFilePath = "serialfiles/aesontest.json"
+testBinarySerialFilePath = "serialfiles/binarytest.serial"
+testCerealSerialFilePath = "serialfiles/cerealtest.serial"
 
 instance ToJSON  TestSerialData where
 instance FromJSON  TestSerialData where   
 
+instance B.Binary TestSerialData where
+instance C.Serialize TestSerialData where
 
 generateTestSerialData ::  [Int] -> [String] -> [(Int, Int)] -> [TestSerialData]
 generateTestSerialData integers strings integerTuples = TestSerialData <$> strings <*> integers <*> integerTuples
@@ -45,6 +55,27 @@ quickCheckAesonSerializeIsIsoMorphic integers strings integerTuples = do
       assert (eTestNowThatFileIsMade == (Right testData))
     _ -> assert (False == True)
 
+quickCheckBinarySerializeIsIsoMorphic integers strings integerTuples = do
+  let testData = generateTestSerialData integers strings integerTuples
+  eTestData <- liftIO $ runBinarySerializationTest testData testBinarySerialFilePath
+  case eTestData of
+    (Left NoFileFound) -> do
+      eTestNowThatFileIsMade <- liftIO $ runBinarySerializationTest testData testBinarySerialFilePath
+      liftIO $ removeFile "serialfiles/binarytest.serial"
+      assert (eTestNowThatFileIsMade == (Right testData))
+    _ -> assert (False == True)
+
+quickCheckCerealSerializeIsIsoMorphic integers strings integerTuples = do
+  let testData = generateTestSerialData integers strings integerTuples
+  eTestData <- liftIO $ runCerealSerializationTest testData testCerealSerialFilePath
+  case eTestData of
+    (Left NoFileFound) -> do
+      eTestNowThatFileIsMade <- liftIO $ runCerealSerializationTest testData testCerealSerialFilePath
+      liftIO $ removeFile "serialfiles/cerealtest.serial"
+      assert (eTestNowThatFileIsMade == (Right testData))
+    _ -> assert (False == True)
+                
+                                
 main :: IO ()
 main = hspec spec
 
@@ -52,8 +83,5 @@ spec :: Spec
 spec = do
   describe "someFunction" $ do
     it "should serialize and deserialize correctly " $ property $ (\s i t -> monadicIO $ quickCheckAesonSerializeIsIsoMorphic s i t)
-
-
-
 testData = generateTestSerialData [0] [""] [(0,0)]
 
